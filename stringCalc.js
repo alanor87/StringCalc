5636806991aa58d4c3c71248b13a6d722df5f5b9
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-06-18 19:42:44 +0300

    Private methods

--- stringCalc.js
+++ stringCalc.js
@@ -16,13 +16,13 @@ export default class StringCalc {
     static rxValOperandTypesCollision = /[a-z]\d|\d[a-z]/i;
     static rxValIncorrectOps = /\*[a-z]|\+\d/i;
 
-    static parenthesisCountVal(str) {
+    static #parenthesisCountVal(str) {
         const openParenth = str.match(/\(/g);
         const closeParenth = str.match(/\)/g);
         return !(openParenth?.length === closeParenth?.length);
     }
 
-    static inputValidation(str) {
+    static #inputValidation(str) {
         const invlaidInputsArray = [
             this.rxValAlienChars.test(str),
             this.rxValStartEnd.test(str),
@@ -31,12 +31,12 @@ export default class StringCalc {
             this.rxValOpParenthCollision.test(str),
             this.rxValOperandTypesCollision.test(str),
             this.rxValIncorrectOps.test(str),
-            this.parenthesisCountVal(str)
+            this.#parenthesisCountVal(str)
         ]
         return invlaidInputsArray.some(value => value === true);
     }
 
-    static multResult(multExpr) {                               //single  op results calculation.
+    static #multResult(multExpr) {                               //single  op results calculation.
         const splitExpr = multExpr.split('*');
         const string = splitExpr[0];
         let newString = '';
@@ -47,52 +47,52 @@ export default class StringCalc {
         return newString;
     }
 
-    static addResult(str) {
+    static #addResult(str) {
         return str.split('+').join('');
     }
 
-    static parenthResult(str) {
+    static #parenthResult(str) {
         return str.match(/[a-z]+/i)
     }
 
-    static multResolve(str) {                                  //total string evaluation for ops, and their resolving.
+    static #multResolve(str) {                                  //total string evaluation for ops, and their resolving.
         let newString = str;
 
-        console.log('Incoming to multResolve() ' + newString);
+        console.log('Incoming to #multResolve() ' + newString);
         console.log('PAttern ' + newString.match(this.rxMultPattern)[0]);
         while (this.rxMultPattern.test(newString)) {
             newString = newString
-                .replace(this.rxMultPattern, this.multResult(newString.match(this.rxMultPattern)[0]));
+                .replace(this.rxMultPattern, this.#multResult(newString.match(this.rxMultPattern)[0]));
         }
         console.log('Result - ' + newString)
         return newString;
     }
 
-    static addResolve(str) {
+    static #addResolve(str) {
         let newString = str;
-        console.log('Incoming to addResolve() ' + newString);
+        console.log('Incoming to #addResolve() ' + newString);
         console.log('PAttern ' + newString.match(this.rxSummPattern)[0]);
         while (this.rxSummPattern.test(newString)) {
             newString = newString
-                .replace(this.rxSummPattern, this.addResult(newString.match(this.rxSummPattern)[0]));
+                .replace(this.rxSummPattern, this.#addResult(newString.match(this.rxSummPattern)[0]));
         }
         console.log('Result - ' + newString)
         return newString;
     }
 
-    static parenthResolve(str) {
+    static #parenthResolve(str) {
         let newString = str;
-        console.log('Incoming to parenthResolve() ' + newString);
+        console.log('Incoming to #parenthResolve() ' + newString);
         console.log('PAttern ' + newString.match(this.rxParenthPattern)[0]);
         while (this.rxParenthPattern.test(newString)) {
             newString = newString
-                .replace(this.rxParenthPattern, this.parenthResult(newString.match(this.rxParenthPattern)[0]));
+                .replace(this.rxParenthPattern, this.#parenthResult(newString.match(this.rxParenthPattern)[0]));
         }
         console.log('Result - ' + newString)
         return newString;
     }
 
-    static errorMsg() {
+    static #errorMsg() {
         console.log('Invalid input!');
         return;
     }
@@ -100,29 +100,29 @@ export default class StringCalc {
     static evaluate(expr) {
 
         if (!expr) {
-            this.errorMsg();
+            this.#errorMsg();
             return 'Invalid input!';
         }
         
         this.inputString = expr.split(' ').join('');
 
-        if (this.inputValidation(this.inputString)) {
-            this.errorMsg();
+        if (this.#inputValidation(this.inputString)) {
+            this.#errorMsg();
             return 'Invalid input!';
         }
 
         while (!this.rxResultPattern.test(this.inputString))                    //start processing input. While we dont have plain letters - keep processing.
         {
             if (this.rxMultPattern.test(this.inputString)) {                   //scanning string for all solid single multiplication ops.
-                this.inputString = this.multResolve(this.inputString);              //resolving multiplication expressions.
+                this.inputString = this.#multResolve(this.inputString);              //resolving multiplication expressions.
             }
 
             if (this.rxSummPattern.test(this.inputString)) {                  //scanning string for addition ops (letters + letters).
-                this.inputString = this.addResolve(this.inputString);
+                this.inputString = this.#addResolve(this.inputString);
             }
 
             if (this.rxParenthPattern.test(this.inputString)) {               //scanning string for expressions in parentheses.
-                this.inputString = this.parenthResolve(this.inputString);          //removing parentheses if all ops inside are settled (letters inside only).
+                this.inputString = this.#parenthResolve(this.inputString);          //removing parentheses if all ops inside are settled (letters inside only).
             }
 
         }
